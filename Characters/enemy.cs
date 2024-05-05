2952868d8360c4346947897d42e255f59a3c1b1e
Marvinfink <129737552+Marvinfink@users.noreply.github.com>
2024-05-05 17:03:34 +0200

    Addeed enemy without fighting

--- Characters/enemy.cs
+++ Characters/enemy.cs
@@ -1,6 +1,42 @@
 using Godot;
 using System;
 
+
 public partial class enemy : CharacterBody2D
 {
+  public int Speed = 60;
+  public bool playerChase = false;
+  public Node2D player = null;
+
+  public override void _PhysicsProcess(double _delta)
+  {
+	AnimationTree animationTree = GetNode<AnimationTree>("AnimationTree");
+	if (playerChase)
+	{
+	  Position += (player.Position - Position) / Speed;
+	  animationTree.Set("parameters/conditions/is_walking", true);
+	  animationTree.Set("parameters/conditions/idle", false);
+	  MoveAndCollide(Vector2.Zero);
+	}
+	else
+	{
+	  animationTree.Set("parameters/conditions/is_walking", false);
+	  animationTree.Set("parameters/conditions/idle", true);
+	}
+  }
+
+  //Chase player when in zone
+  private void _on_detection_area_body_entered(Node2D body)
+  {
+	player = body;
+	playerChase = true;
+  }
+  //stop chasing when not in zone
+  private void _on_detection_area_body_exited(Node2D body)
+  {
+	player = null;
+	playerChase = false;
+  }
+
 }
+
