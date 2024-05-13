b9fae457ba8c1ffb56fe51ad85f37ce2fff4c4bb
Marvinfink <129737552+Marvinfink@users.noreply.github.com>
2024-05-13 22:30:23 +0200

    player

--- Characters/player.gd
+++ Characters/player.gd
@@ -4,11 +4,20 @@ var speed:int=100;
 
 var direction : Vector2 = Vector2.ZERO
 var swing : bool = false
-var walking:bool=false
+var walking : bool=false
+
+var in_attack_range : bool = false
+var enemy_attack_cooldown = false
+var health : int = 100
+var player_alive : bool = true
 
 @onready var animation_tree = $AnimationTree
 
 func _physics_process(_delta):
+	if health <=0:
+		player_alive=false
+		health=0
+		print("player is died")
 	if not swing:
 		velocity = direction * speed
 	else:
@@ -48,3 +57,25 @@ func update_blend_position():
 	animation_tree["parameters/idle/blend_position"] = direction
 	animation_tree["parameters/walk/blend_position"] = direction
 
+func player():
+	pass
+
+func _on_player_hitbox_body_entered(body):
+	if body.has_method("enemy"):
+		print("player took damage")
+		in_attack_range=true
+
+func _on_player_hitbox_body_exited(body):
+	if body.has_method("enemy"):
+		in_attack_range=false
+		
+func enemy_attack():
+	if in_attack_range and enemy_attack_cooldown == true:
+		health = health-20
+		enemy_attack_cooldown =false
+		$attack_cooldown.start()
+		print(health)
+		
+func _on_attack_cooldown_timeout():
+	enemy_attack_cooldown=true
+	
