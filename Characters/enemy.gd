0bd8e8b7265ba045163108e0fe528933af585772
Marvinfink <129737552+Marvinfink@users.noreply.github.com>
2024-05-15 18:46:27 +0200

    death animation and fight method

--- Characters/enemy.gd
+++ Characters/enemy.gd
@@ -5,55 +5,69 @@ var playerChase: bool = false
 var player = null
 var damage = 50
 var element = 1
-var live = 500
+var health = 200
 var armor = 10
 var cooldown = 0
 var enemy_attack_cooldown = true
-var in_range = false
+var enemy_death_cooldown=false
+var player_in_range = false
 
 @onready var animation_tree = $AnimationTree
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	player = get_node("../Player1")
-	pass # Replace with function body.
-
-
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if in_range and enemy_attack_cooldown:
+	if health <=0:
+		health=0
+		print("enemy is died")
+		animation_tree["parameters/conditions/death"] = true
+		enemy_death_cooldown=true
+		
+	if player_in_range and enemy_attack_cooldown:
 		attack()
 
-
 func _physics_process(delta: float) -> void:
-	if playerChase:
-		position += (player.position - position) / Speed
-		animation_tree["parameters/conditions/is_walking"] = true
-		animation_tree["parameters/conditions/idle"] = false
-		animation_tree
-		move_and_collide(Vector2.ZERO)
+	if enemy_death_cooldown:
+		set_process(false)
+		await get_tree().create_timer(2.0).timeout
+		self.queue_free()
 	else:
-		animation_tree["parameters/conditions/is_walking"] = false
-		animation_tree["parameters/conditions/idle"] = true
+		if playerChase:
+			position += (player.position - position) / Speed
+			animation_tree["parameters/conditions/is_walking"] = true
+			animation_tree["parameters/conditions/idle"] = false
+			animation_tree
+			move_and_collide(Vector2.ZERO)
+		else:
+			animation_tree["parameters/conditions/is_walking"] = false
+			animation_tree["parameters/conditions/idle"] = true
 
-
+#check if player has entered/exited hitbox => in attack range
 func _on_enemy_hitbox_body_entered(body):
 	if body.has_method("player"):
-		in_range = true
-	
-
+		player_in_range = true	
 func _on_enemy_hitbox_body_exited(body):
 	if body.has_method("player"):
-		in_range = false
+		player_in_range = false
 	
 func enemy():
 	pass
 	
+#call enemy_attack func from player to do damage
 func attack():
 	enemy_attack_cooldown = false
 	$attack_cooldown.start()
-	player.enemy_attack()
+	player.enemy_attack(damage)
 
+#take damage from player
+func player_attack(damage):
+		health = health-damage
+		#print(health)
+
+#if player in detection area => chase
 func _on_detection_area_body_entered(body):
 	player = body
 	playerChase = true
@@ -63,3 +77,4 @@ func _on_detection_area_body_exited(body):
 
 func _on_attack_cooldown_timeout():
 	enemy_attack_cooldown=true
+	
--- Characters/player.gd
+++ Characters/player.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+var enemy = null
 var speed:int=100;
 
 var direction : Vector2 = Vector2.ZERO
@@ -8,17 +9,23 @@ var walking : bool=false
 
 var in_attack_range : bool = false
 var enemy_attack_cooldown :bool = true
-var health : int = 100
+var health : int = 200
 var player_alive : bool = true
+var damage : int =200
 
 @onready var animation_tree = $AnimationTree
+
+#func _ready() -> void:
+#	enemy = get_node("../Enemy")
 
 func _physics_process(delta):
 	if health <=0:
 		player_alive=false
 		health=0
 		print("player is died")
-		self.queue_free()
+		animation_tree["parameters/conditions/death"] = true
+	if in_attack_range and Input.is_action_just_pressed("swing"):
+		attack(damage)
 	if not swing:
 		velocity = direction * speed
 	else:
@@ -44,15 +51,15 @@ func _process(delta):
 		set_swing(true)
 		animation_tree["parameters/conditions/idle"] = true
 
-		
+#play attack animation
 func set_swing(value = false):
 	swing = value
 	animation_tree["parameters/conditions/swing"] = value
-
+#play walking animation
 func set_walking(value):
 	animation_tree["parameters/conditions/is_walking"] = value
 	animation_tree["parameters/conditions/idle"] = not value
-
+#set animation directon
 func update_blend_position():
 	animation_tree["parameters/attack/blend_position"] = direction
 	animation_tree["parameters/idle/blend_position"] = direction
@@ -61,20 +68,26 @@ func update_blend_position():
 func player():
 	pass
 
-func _on_player_hitbox_body_entered(body):
-	if body.has_method("enemy"):
-		in_attack_range=true
-
-func _on_player_hitbox_body_exited(body):
-	if body.has_method("enemy"):
-		in_attack_range=false
-		
-func enemy_attack():
-		health = health-20
+#take damage from enemy
+func enemy_attack(damage):
+		health = health-damage
 		print(health)
 		enemy_attack_cooldown = false
 		$attack_cooldown.start()
 		
+func attack(damage):
+	enemy.player_attack(damage)		
+
+#check for enemy if in range
+func _on_player_hitbox_body_entered(body):
+	enemy=body
+	if body.has_method("enemy"):
+		in_attack_range=true
+#check for enemy if out of range
+func _on_player_hitbox_body_exited(body):
+	if body.has_method("enemy"):
+		in_attack_range=false
+			
 func _on_attack_cooldown_timeout():
 	enemy_attack_cooldown = true
 	
