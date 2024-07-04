dff0231f5e667ccee8448a5fe440c3e55af06dee
the-andyg <100201636+the-andyg@users.noreply.github.com>
2024-07-04 23:09:05 +0200

    add spells for player

--- Characters/player/player.gd
+++ Characters/player/player.gd
@@ -1,6 +1,5 @@
 extends CharacterBody2D
 
-
 #playerstats
 var health: int = 10
 var speed:int=100;
@@ -57,12 +56,16 @@ var element_textures = {
 	Elements.Element.PLANT: preload("res://Art/mystic_woods_free_2.1/sprites/characters/Plant/Sprite_Plant_Complete_Outlines-Sheet.png")
 }
 
-const player_projectile := preload("res://Characters/projectile/player_projectile.tscn")
+@onready var electricity_projectile := preload("res://enemy/projectile/bolt.tscn")
+@onready var water_projectile := preload("res://enemy/projectile/wave.tscn")
+@onready var fire_projectile := preload("res://enemy/projectile/fire_tornado.tscn")
+@onready var plant_projectile := preload("res://enemy/projectile/thorns.tscn")
 
 func _ready():
 	player_node=get_node(".")
 	health_bar.set_max_hearts(health)
-	
+
+
 #Handles the user input
 func _input(event):
 	# handle dash input
@@ -73,37 +76,28 @@ func _input(event):
 	# handle swing input
 	if in_attack_range and Input.is_action_just_pressed("swing") and enemy_attack_cooldown == true:
 		attack(damage)
-		
-		
 	# handle special attack input
-	if Input.is_action_just_pressed("special_attack") and mana_handler.use_spell():
+	if Input.is_action_just_pressed("special_attack"):
 		handle_special_attack()
-	#change element
-	if Input.is_action_just_pressed("fire"):
-		element=Elements.Element.FIRE
-		change_element()
-	elif Input.is_action_just_pressed("water"):
-		element=Elements.Element.WATER
-		change_element()
-	elif Input.is_action_just_pressed("electro"):
-		element=Elements.Element.ELECTRICITY
-		change_element()
-	elif Input.is_action_just_pressed("plant"):
-		element=Elements.Element.PLANT
-		change_element()
-
 	#swing while walking
 	if Input.is_action_just_pressed("swing"):# and walking:
 		set_swing(true)
 		swingSoundCharacter.play()
-		
+	#change element
+	if Input.is_action_just_pressed("fire"):
+		element=Elements.Element.FIRE
+	elif Input.is_action_just_pressed("water"):
+		element=Elements.Element.WATER
+	elif Input.is_action_just_pressed("electro"):
+		element=Elements.Element.ELECTRICITY
+	elif Input.is_action_just_pressed("plant"):
+		element=Elements.Element.PLANT
+	$Sprite2D2.texture = element_textures[element]
 
-		
-	
+
 func _physics_process(delta):
 	if animation_tree1.get("parameters/conditions/dash"):
 		return
-
 	#walking
 	direction = Input.get_vector("left","right","up","down")
 	if not swing:
@@ -112,7 +106,6 @@ func _physics_process(delta):
 		update_blend_position()
 	else:
 		velocity = Vector2.ZERO
-	
 	if direction != Vector2.ZERO and not swing:
 		if not walking:
 			walkingSound.play()
@@ -125,14 +118,11 @@ func _physics_process(delta):
 			walkingSound.stop()
 		walking=false
 		set_walking(walking)
-		
 	if dash_used:
 		handle_dash(delta)
 	move_and_slide()
-	
-	
-	
-	
+
+
 # attack functions
 func attack(damage):
 	mana_handler.load_spell()
@@ -140,59 +130,83 @@ func attack(damage):
 		enemy_attack_cooldown = false
 		$attack_cooldown.start()
 		enemy.player_attack(damage,element)	
-		
-func special_attack(special_damage):
-	enemy.player_attack(special_damage,element)
-	
-func special_attack_range():
-	var projectile := player_projectile.instantiate()
-	owner.add_child(projectile)
-	projectile.element=element
-	projectile_direction = (get_global_mouse_position() - global_position).normalized()
-	projectile.position=global_position
-	projectile.rotation = self.global_position.direction_to(get_global_mouse_position()).angle()
-	
-func special_attack_close():
-	if special_in_range:
-		special_attack(special_damage)
+
 
 func handle_special_attack():
-	if element == 0:
-		print("Fire special")
-		special_attack_close()
-		deathSound.play()
-	elif element == 1:
-		print("water special")
-		special_attack_range()
-		#start animation
-	elif element == 2:
-		print("plant special")
-		special_attack_range()
-		#start animation
-	else:
-		print("elctro special")
-		special_attack_close()
-		#start animation
+	match element:
+		Elements.Element.FIRE:
+			if spells_available[Elements.Element.FIRE] and mana_handler.use_spell():
+				use_fire_spell()
+		Elements.Element.WATER:
+			if spells_available[Elements.Element.WATER] and mana_handler.use_spell():
+				use_water_spell()
+		Elements.Element.ELECTRICITY:
+			if spells_available[Elements.Element.ELECTRICITY] and mana_handler.use_spell():
+				use_electricity_spell()
+		Elements.Element.PLANT:
+			if spells_available[Elements.Element.PLANT] and mana_handler.use_spell():
+				use_plant_spell()
 
-# Change element and sprite texture
-func change_element():
-	$Sprite2D2.texture = element_textures[element]
+
+func use_fire_spell():
+	var projectile := fire_projectile.instantiate()
+	projectile.global_position = self.global_position
+	projectile.set_direction(get_global_mouse_position() - global_position)
+	projectile.created_by_player = true
+	projectile.damage = damage
+	projectile.range = 150
+	owner.add_child(projectile)
+
+
+func use_water_spell():
+	var projectile := water_projectile.instantiate()
+	var angle = global_position.direction_to(get_global_mouse_position()).angle()
+	if abs(angle) >= PI/2 :
+		projectile.scale.y = -1
+	projectile.global_position = self.global_position
+	projectile.set_direction(get_global_mouse_position() - global_position)
+	projectile.rotation = angle
+	projectile.created_by_player = true
+	projectile.damage = damage
+	owner.add_child(projectile)
+
+
+func use_plant_spell():
+	var projectile := plant_projectile.instantiate()
+	projectile.created_by_player = true
+	projectile.global_position = get_global_mouse_position()
+	projectile.damage = damage
+	owner.add_child(projectile)
+
+
+func use_electricity_spell():
+	var angle = global_position.direction_to(get_global_mouse_position()).angle()
+	var projectile := electricity_projectile.instantiate()
+	var position = self.global_position
+	var half_width = 60
+	var offset = Vector2(half_width, 0).rotated(angle)
+	projectile.rotation = angle
+	projectile.global_position = position + offset
+	projectile.created_by_player = true
+	projectile.damage = damage
+	owner.add_child(projectile)
+
 
 # Changes player direction to mouse position
 func player_looking():	
 	player_position=player_node.position
 	mouse_position= get_global_mouse_position()
-
 	player_direction=mouse_position-player_position
 	player_direction=player_direction.normalized()
 	$player_hitbox/CollisionPolygon2D.look_at(get_global_mouse_position())
-	
-	
+
+
 # dash functions
 func dash():
 	mouse_position=get_global_mouse_position()
 	dash_direction = (mouse_position - global_position).normalized()
 	set_dash(true)
+
 
 func handle_dash(delta):
 	dash_timer+=delta
@@ -213,7 +227,7 @@ func set_swing(value = false):
 	set_physics_process(false)
 	await get_tree().create_timer(0.35).timeout
 	set_physics_process(true)
-	
+
 
 # walking animation
 func set_walking(value):
@@ -223,8 +237,7 @@ func set_walking(value):
 #dash animation
 func set_dash(value = false):
 	animation_tree1["parameters/conditions/dash"]=value
-	
-		
+
 
 #damage animation
 func set_damage(value = false):
@@ -232,8 +245,8 @@ func set_damage(value = false):
 	set_physics_process(false)
 	await get_tree().create_timer(0.35).timeout
 	set_physics_process(true)
-	
-	
+
+
 # set animation direction
 func update_blend_position():
 	animation_tree1["parameters/attack/blend_position"] = player_direction
@@ -245,7 +258,6 @@ func update_blend_position():
 
 #take damage from enemy
 func enemy_attack():
-	
 	health -= 1
 	health_bar.set_heart_bar(health)
 	set_damage(true)
@@ -258,8 +270,8 @@ func enemy_attack():
 		await get_tree().create_timer(3.0).timeout
 		get_node("../Game_Over_Overlay").game_over()
 	print(health)
-	
-	
+
+
 func set_first_element(e: Elements.Element):
 	element = e
 	spells_available.resize(Elements.Element.size())
@@ -267,8 +279,8 @@ func set_first_element(e: Elements.Element):
 	for elem in Elements.Element.values():
 		spells_available[elem] = false
 	spells_available[e] = true
-	
-	
+
+
 func endboss_killed(el: Elements.Element):
 	spells_available[el] = true
 	var check = false
@@ -279,32 +291,39 @@ func endboss_killed(el: Elements.Element):
 			health_bar.set_heart_bar(health)
 			return
 	get_node("../Winning_Overlay").show_winning_screen()
-	
+
 
 func player():
 	pass
+
 
 # check for enemy if in range
 func _on_player_hitbox_body_entered(body):
 	if body.has_method("enemy"):
 		enemy=body
 		in_attack_range=true
+
+
 # check for enemy if out of range
 func _on_player_hitbox_body_exited(body):
 	if body.has_method("enemy"):
 		in_attack_range=false
+
+
 #check for enemy in special attack range
 func _on_special_attack_body_entered(body):
 	enemy=body
 	if body.has_method("enemy"):
 		special_in_range = true
+
+
 #check for enemy out of special attack range
 func _on_special_attack_body_exited(body):
 	if body.has_method("enemy"):
 		special_in_range = false
-# attack cooldown			
+
+
+# attack cooldown	
 func _on_attack_cooldown_timeout():
 	enemy_attack_cooldown = true
 # special attack cooldown	
-
-
